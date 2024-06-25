d20b7fec95dfe7fd3d9ad6ea7d610bf961f452e7
BraulioDavidMolina <ti5amolinafloresbrauliodavid@gmail.com>
2024-06-24 19:57:40 -0600

    Modulos: Alumnos, Comentarios, Comentarios desde el form

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
-from .models import Alumno
+from .models import Alumnos
+from .models import Comentario
+from .models import ComentarioContacto
 
 # Register your models here.
-admin.site.register(Alumno)
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('matricula', 'nombre', 'carrera', 'turno')
+    search_fields = ('matricula', 'nombre', 'carrera', 'turno')
+    date_hierarchy = 'created'
+    list_filter = ('carrera','turno')
+
+
+class AdministrarComentarios(admin.ModelAdmin):
+    list_display = ('id', 'coment')
+    search_fields = ('id', 'created')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+    #list_filter = ('carrera','turno')
+
+class AdministrarComentarioContacto(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'mensaje', 'created')
+    search_fields = ('id', 'comentario')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+    #list_filter = ('carrera','turno')
+    
+admin.site.register(Alumnos, AdministrarModelo)
+admin.site.register(Comentario, AdministrarComentarios)
+admin.site.register(ComentarioContacto, AdministrarComentarioContacto)
+
 
